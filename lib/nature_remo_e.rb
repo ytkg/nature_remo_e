726ff27349d4f59e1045082b6f4724bebfd1442e
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-01-06 21:50:45 +0900

    first commit

---
+++ lib/nature_remo_e.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nature_remo_e/version"
+
+module NatureRemoE
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nature_remo_e/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NatureRemoE
+  VERSION = "0.1.0"
+end
