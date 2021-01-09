a9758f13fc21a881197f74b63244205c64169002
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-01-09 18:17:36 +0900

    [add] メインの機能実装

---
+++ lib/nature_remo_e/client.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'faraday'
+require 'json'
+
+module NatureRemoE
+  class Client
+    def initialize(token)
+      @token = token
+    end
+
+    def echonetlite_properties
+      device[:smart_meter][:echonetlite_properties].map do |echonetlite_property|
+        [echonetlite_property[:name].to_sym, echonetlite_property[:val].to_i]
+      end.to_h
+    end
+
+    def coefficient
+      echonetlite_properties[:coefficient]
+    end
+
+    def cumulative_electric_energy_effective_digits
+      echonetlite_properties[:cumulative_electric_energy_effective_digits]
+    end
+
+    def normal_direction_cumulative_electric_energy
+      echonetlite_properties[:normal_direction_cumulative_electric_energy]
+    end
+
+    def cumulative_electric_energy_unit
+      echonetlite_properties[:cumulative_electric_energy_unit]
+    end
+
+    def reverse_direction_cumulative_electric_energy
+      echonetlite_properties[:reverse_direction_cumulative_electric_energy]
+    end
+
+    def measured_instantaneous
+      echonetlite_properties[:measured_instantaneous]
+    end
+
+    private
+
+    def client
+      @client ||= Faraday.new(
+        url: 'https://api.nature.global',
+        headers: { Authorization: "Bearer #{@token}" }
+      )
+    end
+
+    def appliances
+      response = client.get do |req|
+        req.url '/1/appliances'
+      end
+
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def device
+      appliances.find { |appliance| appliance[:type] == 'EL_SMART_METER' }
+    end
+  end
+end
